f5ef4f5c7eedbbe6ff165d919e57d0e14e019260
Christoph Reiter <reiter.christoph@gmail.com>
2017-09-10 12:01:49 +0200

    show outdated packages

--- main.py
+++ main.py
@@ -26,24 +26,35 @@ import contextlib
 import datetime
 import io
 import re
+import os
 import tarfile
 import threading
 import time
+from itertools import zip_longest
 
 import requests
 from flask import Flask, render_template, request, url_for
 
 
-CONFIG = {
-    "http://repo.msys2.org/mingw/i686/mingw32.files": ("mingw32", ""),
-    "http://repo.msys2.org/mingw/x86_64/mingw64.files": ("mingw64", ""),
-    "http://repo.msys2.org/msys/i686/msys.files": ("msys", "i686"),
-    "http://repo.msys2.org/msys/x86_64/msys.files": ("msys", "x86_64"),
-}
+CONFIG = [
+    ("http://repo.msys2.org/mingw/i686/mingw32.files", "mingw32", ""),
+    ("http://repo.msys2.org/mingw/x86_64/mingw64.files", "mingw64", ""),
+    ("http://repo.msys2.org/msys/i686/msys.files", "msys", "i686"),
+    ("http://repo.msys2.org/msys/x86_64/msys.files", "msys", "x86_64"),
+]
+
+VERSION_CONFIG = []
+for repo in ["core", "extra", "community", "testing", "community-testing"]:
+    VERSION_CONFIG.append(
+        ("http://ftp.halifax.rwth-aachen.de/archlinux/"
+         "{0}/os/x86_64/{0}.db".format(repo), repo, ""))
 
 UPDATE_INTERVAL = 60 * 15
+CACHE_LOCAL = False
 
 sources = []
+versions = {}
+last_update = 0
 
 app = Flask(__name__)
 
@@ -99,6 +110,12 @@ class Package:
         return "Package(%s)" % self.fileurl
 
     @property
+    def realname(self):
+        if self.repo.startswith("mingw"):
+            return self.name.split("-", 3)[-1]
+        return self.name
+
+    @property
     def key(self):
         return (self.repo, self.repo_variant,
                 self.name, self.arch, self.fileurl)
@@ -132,6 +149,20 @@ class Source:
         self.groups = groups
 
         self.packages = {}
+
+    @property
+    def realname(self):
+        if self.repo.startswith("mingw"):
+            return self.name.split("-", 2)[-1]
+        return self.name
+
+    @property
+    def date(self):
+        """One of the package build dates"""
+
+        for n, p in sorted(self.packages.items()):
+            return p.builddate
+        return 0
 
     @property
     def source_url(self):
@@ -189,8 +220,19 @@ def parse_repo(repo, repo_variant, url):
 
         source.add_desc(d, base_url)
 
-    r = requests.get(url)
-    with io.BytesIO(r.content) as f:
+    if CACHE_LOCAL:
+        fn = url.replace("/", "_")
+        if not os.path.exists(fn):
+            r = requests.get(url)
+            with open(fn, "wb") as h:
+                h.write(r.content)
+        with open(fn, "rb") as h:
+            data = h.read()
+    else:
+        r = requests.get(url)
+        data = r.content
+
+    with io.BytesIO(data) as f:
         with tarfile.open(fileobj=f, mode="r:gz") as tar:
             packages = {}
             for info in tar.getmembers():
@@ -252,11 +294,16 @@ def funcs():
         name = name or package.name
         return name[len(re.split("[<>=]+", name)[0]):].strip()
 
+    def update_timestamp():
+        return last_update
+
     return dict(package_url=package_url, package_name=package_name,
-                package_restriction=package_restriction)
+                package_restriction=package_restriction,
+                update_timestamp=update_timestamp)
 
 
 @app.route('/')
+@app.route('/base')
 @app.route('/base/<name>')
 def base(name=None):
     global sources
@@ -316,6 +363,254 @@ def updates():
     return render_template('updates.html', packages=packages[:150])
 
 
+def package_name_is_vcs(package_name):
+    return package_name.endswith(
+        ("-cvs", "-svn", "-hg", "-darcs", "-bzr", "-git"))
+
+
+def get_arch_name(name):
+    mapping = {
+        "freetype": "freetype2",
+        "lzo2": "lzo",
+        "liblzo2": "lzo",
+        "python-bsddb3": "python-bsddb",
+        "graphite2": "graphite",
+        "mpc": "libmpc",
+        "eigen3": "eigen",
+        "python-icu": "python-pyicu",
+        "python-bsddb3": "python-bsddb",
+        "python3": "python",
+        "sqlite3": "sqlite",
+        "gexiv2": "libgexiv2",
+        "webkitgtk3": "webkitgtk",
+        "python2-nuitka": "nuitka",
+        "python2-ipython": "ipython",
+        "openssl": "openssl-1.0",
+        "gtksourceviewmm3": "gtksourceviewmm",
+        "librest": "rest",
+        "gcc-libgfortran": "gcc-fortran",
+        "meld3": "meld",
+        "antlr3": "libantlr3c",
+        "geoclue": "geoclue2",
+        "python-zope.event": "python-zope-event",
+        "python-zope.interface": "python-zope-interface",
+        "tesseract-ocr": "tesseract",
+    }
+
+    name = name.lower()
+
+    if name.startswith("python3-"):
+        name = name.replace("python3-", "python-")
+
+    if name in mapping:
+        return mapping[name]
+
+    return name
+
+
+def vercmp(v1, v2):
+
+    def cmp(a, b):
+        return (a > b) - (a < b)
+
+    def split(v):
+        e, v = v.split("~", 1) if "~" in v else ("0", v)
+        v, r = v.rsplit("-", 1) if "-" in v else (v, None)
+        return (e, v, r)
+
+    digit, alpha, other = range(3)
+
+    def get_type(c):
+        assert c
+        if c.isdigit():
+            return digit
+        elif c.isalpha():
+            return alpha
+        else:
+            return other
+
+    def parse(v):
+        parts = []
+        seps = 0
+        current = ""
+        for c in v:
+            if get_type(c) == other:
+                if current:
+                    parts.append((seps, current))
+                    current = ""
+                seps += 1
+            else:
+                if not current:
+                    current += c
+                else:
+                    if get_type(c) == get_type(current):
+                        current += c
+                    else:
+                        parts.append((seps, current))
+                        current = c
+
+        parts.append((seps, current or None))
+
+        return parts
+
+    def rpmvercmp(v1, v2):
+        for (s1, p1), (s2, p2) in zip_longest(parse(v1), parse(v2),
+                                              fillvalue=(None, None)):
+
+            if s1 is not None and s2 is not None:
+                ret = cmp(s1, s2)
+                if ret != 0:
+                    return ret
+
+            if p1 is None and p2 is None:
+                return 0
+
+            if p1 is None:
+                if get_type(p2) == alpha:
+                    return 1
+                return -1
+            elif p2 is None:
+                if get_type(p1) == alpha:
+                    return -1
+                return 1
+
+            t1 = get_type(p1)
+            t2 = get_type(p2)
+            if t1 != t2:
+                if t1 == digit:
+                    return 1
+                elif t2 == digit:
+                    return -1
+            elif t1 == digit:
+                ret = cmp(int(p1), int(p2))
+                if ret != 0:
+                    return ret
+            elif t1 == alpha:
+                ret = cmp(p1, p2)
+                if ret != 0:
+                    return ret
+
+        return 0
+
+    e1, v1, r1 = split(v1)
+    e2, v2, r2 = split(v2)
+
+    ret = rpmvercmp(e1, e2)
+    if ret == 0:
+        ret = rpmvercmp(v1, v2)
+        if ret == 0 and r1 is not None and r2 is not None:
+            ret = rpmvercmp(r1, r2)
+
+    return ret
+
+
+def arch_version_to_msys(v):
+    return v.replace(":", "~")
+
+
+def version_is_newer_than(v1, v2):
+    return vercmp(v1, v2) == 1
+
+
+def update_versions():
+    global VERSION_CONFIG, versions, sources
+
+    print("update versions")
+    arch_versions = {}
+    for (url, repo, variant) in VERSION_CONFIG:
+        for source in parse_repo(repo, variant, url).values():
+            msys_ver = arch_version_to_msys(source.version)
+            for p in source.packages.values():
+                url = "https://www.archlinux.org/packages/%s/%s/%s/" % (
+                    p.repo, p.arch, p.name)
+
+                if p.name in arch_versions:
+                    old_ver = arch_versions[p.name][0]
+                    if version_is_newer_than(msys_ver, old_ver):
+                        arch_versions[p.name] = (msys_ver, url)
+                else:
+                    arch_versions[p.name] = (msys_ver, url)
+    print("done")
+
+    print("update versions from AUR")
+    # a bit hacky, try to get the remaining versions from AUR
+    possible_names = set()
+    for s in sources:
+        if package_name_is_vcs(s.name):
+            continue
+        for p in s.packages.values():
+            possible_names.add(get_arch_name(p.realname))
+        possible_names.add(get_arch_name(s.realname))
+
+    r = requests.get("https://aur.archlinux.org/packages.gz")
+    aur_packages = set()
+    for name in r.text.splitlines():
+        if name.startswith("#"):
+            continue
+        if name in arch_versions:
+            continue
+        if name not in possible_names:
+            continue
+        aur_packages.add(name)
+
+    aur_url = (
+        "https://aur.archlinux.org/rpc/?v=5&type=info&" +
+        "&".join(["arg[]=%s" % n for n in aur_packages]))
+    r = requests.get(aur_url)
+    for result in r.json()["results"]:
+        name = result["Name"]
+        if name not in aur_packages or name in arch_versions:
+            continue
+        url = "https://aur.archlinux.org/packages/%s" % name
+        arch_versions[name] = (result["Version"], url)
+    print("done")
+
+    versions = arch_versions
+
+
+def extract_upstream_version(version):
+    return version.rsplit(
+        "-")[0].split("+", 1)[0].split("~", 1)[-1].split(":", 1)[-1]
+
+
+@app.route('/outofdate')
+def outofdate():
+    global sources, versions
+
+    missing = []
+    to_update = []
+    for s in sources:
+        if package_name_is_vcs(s.name):
+            continue
+
+        variants = [(p.realname, p.version) for p in s.packages.values()]
+        variants.append((s.realname, s.version))
+
+        for realname, version in variants:
+            arch_name = get_arch_name(realname)
+            if arch_name in versions:
+                arch_version, url = versions[arch_name]
+                arch_version = extract_upstream_version(
+                    arch_version)
+                msys_version = extract_upstream_version(version)
+
+                if version_is_newer_than(arch_version, msys_version):
+                    to_update.append((s, msys_version, arch_version, url))
+                break
+        else:
+            missing.append((s, get_arch_name(s.realname)))
+
+    # show packages which have recently been build first.
+    # assumes high frequency update packages are more important
+    to_update.sort(key=lambda i: i[0].date, reverse=True)
+
+    missing.sort(key=lambda i: i[0].name)
+
+    return render_template(
+        'outofdate.html',
+        sources=sources, to_update=to_update, missing=missing)
+
+
 @app.route('/search')
 def search():
     global sources
@@ -335,8 +630,12 @@ def search():
 def check_needs_update(_last_time=[""]):
     """Raises RequestException"""
 
+    if CACHE_LOCAL:
+        yield True
+
     t = ""
-    for url in sorted(CONFIG):
+    for config in sorted(CONFIG + VERSION_CONFIG):
+        url = config[0]
         r = requests.head(url)
         t += r.headers["last-modified"]
 
@@ -353,7 +652,7 @@ def update_source():
     global sources, CONFIG
 
     final = {}
-    for url, (repo, variant) in CONFIG.items():
+    for (url, repo, variant) in CONFIG:
         for name, source in parse_repo(repo, variant, url).items():
             if name in final:
                 final[name].packages.update(source.packages)
@@ -364,19 +663,21 @@ def update_source():
 
 
 def update_thread():
-    global sources, CONFIG, UPDATE_INTERVAL
+    global sources, UPDATE_INTERVAL, last_update
 
     while True:
         try:
             print("check for update")
             with check_needs_update() as needs:
                 if needs:
-                    print("update source")
+                    print("update source/versions")
                     update_source()
+                    update_versions()
                 else:
                     print("not update needed")
         except Exception as e:
             print(e)
+        last_update = time.time()
         print("Sleeping for %d" % UPDATE_INTERVAL)
         time.sleep(UPDATE_INTERVAL)
 
