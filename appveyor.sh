bff21b04e76f5097a3cb3de051870859888020ae
Christoph Reiter <reiter.christoph@gmail.com>
2017-11-14 10:02:24 +0100

    do shallow clones

--- appveyor.sh
+++ appveyor.sh
@@ -4,8 +4,8 @@ set -e
 
 pacman --noconfirm -S --needed mingw-w64-i686-python3
 
-git clone https://github.com/Alexpux/MINGW-packages
-git clone https://github.com/Alexpux/MSYS2-packages
+git clone --depth 1 https://github.com/Alexpux/MINGW-packages
+git clone --depth 1 https://github.com/Alexpux/MSYS2-packages
 curl -o srcinfo.json -L 'https://github.com/lazka/msys2-web/releases/download/cache/srcinfo.json'
 
 python3 update-srcinfo.py srcinfo.json MINGW-packages MSYS2-packages
