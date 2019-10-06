47d7f31d8917ee173a45e96d49da69eba0577fcc
Christoph Reiter <reiter.christoph@gmail.com>
2019-10-06 16:20:26 +0200

    appveyor: retry src update
    git sometimes fails

--- appveyor.sh
+++ appveyor.sh
@@ -6,4 +6,9 @@ git clone https://github.com/msys2/MINGW-packages
 git clone https://github.com/msys2/MSYS2-packages
 curl -o srcinfo.json -L 'https://github.com/msys2/msys2-web/releases/download/cache/srcinfo.json'
 
-python3 -u update-srcinfo.py srcinfo.json MINGW-packages MSYS2-packages
+n=0
+until [ $n -ge 5 ]
+do
+   python3 -u update-srcinfo.py srcinfo.json MINGW-packages MSYS2-packages && break || sleep 10
+   n=$[$n+1]
+done
