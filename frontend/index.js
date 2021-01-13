64bf45e89e2bd973f0511dedc8b39d49c03afd0b
Syrus Dark <naveen@syrusdark.website>
2021-01-14 02:03:18 +0530

    Add a copy button

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,18 @@
 import './style.scss';
 import 'bootstrap';
+
+class App {
+
+    static copyToClipboard(button) {
+        let text = button.parentNode.getElementsByTagName("code")[0].innerText
+        if (navigator.clipboard) {
+            navigator.clipboard.writeText(text);
+            let oldInner = button.innerHTML;
+            button.innerHTML = "✅";
+            setTimeout(() => button.innerHTML = oldInner, 1000);
+        }
+    }
+
+};
+
+window.App = App;
