b69bd0e3f32232be913fae30a7bf27d096aebefe
Christoph Reiter <reiter.christoph@gmail.com>
2021-03-09 08:59:00 +0100

    Install fonts from npm

--- app/static/fonts/README.md
+++
@@ -1,2 +0,0 @@
-From https://google-webfonts-helper.herokuapp.com/fonts/roboto?subsets=latin
-License: Apache-2.0
--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -4,6 +4,7 @@ import {terser} from "rollup-plugin-terser";
 import postcss from 'rollup-plugin-postcss';
 import autoprefixer from 'autoprefixer';
 import replace from '@rollup/plugin-replace';
+import copy from 'rollup-plugin-copy'
 
 const dev = process.env.ROLLUP_WATCH === 'true';
 
@@ -26,6 +27,12 @@ export default {
       plugins: [
         autoprefixer()
       ]
+    }),
+    copy({
+        targets: [
+          { src: 'node_modules/@fontsource/roboto/files/roboto-latin-400-normal.woff*', dest: '../app/static/fonts' },
+          { src: 'node_modules/@fontsource/roboto/files/roboto-latin-700-normal.woff*', dest: '../app/static/fonts' },
+        ]
     }),
     !dev && terser(),
   ],
