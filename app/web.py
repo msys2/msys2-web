msys2/msys2-web

commit bb4b9967260b13fa71f53e200c604c5eab83731f

package index: remove the "all" option and default to some repo
The page is getting very large and showing all packages for all
repos shouldn't be a common use case.

Let's filter always and default to mingw64

app/templates/packages.html
@@ -11,12 +11,11 @@
 
     <form class="row g-2 align-items-center" action="{{ url_for('package') }}" method="get">
         <div class="col-auto">
-            Repositories:
+            Repository:
         </div>
 
         <div class="col-auto">
             <select name="repo" class="form-select form-select-sm" onchange="this.form.submit()">
-              <option {{ "selected" if not repo_filter or "" }} value="">All</option>
               {% for r in repos|sort(attribute='name') %}
               <option value="{{ r.name }}" {{ "selected" if repo_filter == r.name or "" }}>{{ r.name }}</option>
               {% endfor %}

app/web.py
@@ -21,6 +21,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi_etag import add_exception_handler as add_etag_exception_handler
 
 from .appstate import state, get_repositories, Package, is_skipped, Source, DepType, SrcInfoPackage
+from .appconfig import DEFAULT_REPO
 from .utils import extract_upstream_version, version_is_newer_than
 
 router = APIRouter(default_response_class=HTMLResponse)
@@ -271,13 +272,15 @@ async def group(request: Request, response: Response, group_name: Optional[str] 
 async def package(request: Request, response: Response, package_name: Optional[str] = None, repo: Optional[str] = None, variant: Optional[str] = None) -> Response:
     global state
 
+    repo = repo or DEFAULT_REPO
+
     packages = []
     provides = []
     for s in state.sources.values():
         for k, p in sorted(s.packages.items()):
             is_package_exact = (package_name is None or p.name == package_name)
             if is_package_exact or package_name in p.provides:
-                if not repo or p.repo == repo:
+                if p.repo == repo:
                     if not variant or p.repo_variant == variant:
                         if is_package_exact:
                             packages.append((s, p))
@@ -298,7 +301,7 @@ async def package(request: Request, response: Response, package_name: Optional[s
             "request": request,
             "packages": packages,
             "repos": repos,
-            "repo_filter": repo or None,
+            "repo_filter": repo,
         }, headers=dict(response.headers))
 
 

app/appconfig.py
@@ -26,6 +26,8 @@ CONFIG = [
     (REPO_URL + "/msys/x86_64/msys.files", "msys", "x86_64"),
 ]
 
+DEFAULT_REPO = "mingw64"
+
 VERSION_CONFIG = []
 for repo in ["core", "extra", "community", "testing", "community-testing",
              "multilib"]:

commit 5beb2670f518c1aaa50671ef2398b7980b723209

Split up the package list and package details logic
The change in bb4b9967260 broke the package details view since
it defaults the repo filter to mingw64, so clang64 links are broken now.

This now only applies the default for the list.

To avoid mixing things up in the future split up the endpoint logic.

Fixes #55

app/templates/navbar.html
@@ -15,7 +15,7 @@
     <a class="nav-link {{ 'active' if is_endpoint('base') else '' }}" href="{{ url_for('base') }}">Base Packages</a>
 </li>
 <li class="nav-item">
-    <a class="nav-link {{ 'active' if is_endpoint('package') else '' }}" href="{{ url_for('package') }}">Packages</a>
+    <a class="nav-link {{ 'active' if is_endpoint('package') else '' }}" href="{{ url_for('packages') }}">Packages</a>
   </li>
 <li class="nav-item">
   <a class="nav-link {{ 'active' if is_endpoint('group') else '' }}" href="{{ url_for('group') }}">Groups</a>

app/templates/packages.html
@@ -9,7 +9,7 @@
   </div>
   <div class="card-body overflow-auto">
 
-    <form class="row g-2 align-items-center" action="{{ url_for('package') }}" method="get">
+    <form class="row g-2 align-items-center" action="{{ url_for('packages') }}" method="get">
         <div class="col-auto">
             Repository:
         </div>

app/templates/repos.html
@@ -20,7 +20,7 @@
       <dd class="col-sm-9"><a href="{{ repo.src_url }}">{{ repo.src_url }}</a></dd>
 
       <dt class="col-sm-3 text-sm-end">Packages:</dt>
-      <dd class="col-sm-9"><a href="{{ url_for('package') }}?repo={{ repo.name }}">{{ repo.packages|length }}</a></dd>
+      <dd class="col-sm-9"><a href="{{ url_for('packages') }}?repo={{ repo.name }}">{{ repo.packages|length }}</a></dd>
 
       <dt class="col-sm-3 text-sm-end">Package Size:</dt>
       <dd class="col-sm-9">{{ repo.csize|filesize }}</dd>

app/web.py
@@ -268,11 +268,30 @@ async def group(request: Request, response: Response, group_name: Optional[str] 
 
 
 @router.get('/package/', dependencies=[Depends(Etag(get_etag))])
-@router.get('/package/{package_name}', dependencies=[Depends(Etag(get_etag))])
-async def package(request: Request, response: Response, package_name: Optional[str] = None, repo: Optional[str] = None, variant: Optional[str] = None) -> Response:
+async def packages(request: Request, response: Response, repo: Optional[str] = None, variant: Optional[str] = None) -> Response:
     global state
 
     repo = repo or DEFAULT_REPO
+
+    packages = []
+    for s in state.sources.values():
+        for k, p in sorted(s.packages.items()):
+            if p.repo == repo:
+                if not variant or p.repo_variant == variant:
+                    packages.append((s, p))
+
+    repos = get_repositories()
+    return templates.TemplateResponse("packages.html", {
+        "request": request,
+        "packages": packages,
+        "repos": repos,
+        "repo_filter": repo,
+    }, headers=dict(response.headers))
+
+
+@router.get('/package/{package_name}', dependencies=[Depends(Etag(get_etag))])
+async def package(request: Request, response: Response, package_name: str, repo: Optional[str] = None, variant: Optional[str] = None) -> Response:
+    global state
 
     packages = []
     provides = []
@@ -280,7 +299,7 @@ async def package(request: Request, response: Response, package_name: Optional[s
         for k, p in sorted(s.packages.items()):
             is_package_exact = (package_name is None or p.name == package_name)
             if is_package_exact or package_name in p.provides:
-                if p.repo == repo:
+                if not repo or p.repo == repo:
                     if not variant or p.repo_variant == variant:
                         if is_package_exact:
                             packages.append((s, p))
@@ -290,19 +309,10 @@ async def package(request: Request, response: Response, package_name: Optional[s
     # show the real package always first
     packages.extend(provides)
 
-    if package_name is not None:
-        return templates.TemplateResponse("package.html", {
-            "request": request,
-            "packages": packages,
-        }, headers=dict(response.headers))
-    else:
-        repos = get_repositories()
-        return templates.TemplateResponse("packages.html", {
-            "request": request,
-            "packages": packages,
-            "repos": repos,
-            "repo_filter": repo,
-        }, headers=dict(response.headers))
+    return templates.TemplateResponse("package.html", {
+        "request": request,
+        "packages": packages,
+    }, headers=dict(response.headers))
 
 
 @router.get('/updates', dependencies=[Depends(Etag(get_etag))])